9c33bb013502b0811afc5eb1b028148750ac46af
vkh <vl@d.d>
2024-10-04 05:12:32 +0300

    deprecate весь функционал с кнопками и менюшками в лс

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,14 +2,10 @@ from aiogram import Router
 
 
 def get_handlers_router() -> Router:
-    from . import export_users, info, menu, start, support, inline_mode
+    from . import export_users, start
 
     router = Router()
     router.include_router(start.router)
-    router.include_router(info.router)
-    router.include_router(support.router)
-    router.include_router(menu.router)
     router.include_router(export_users.router)
-    router.include_router(inline_mode.router)
 
     return router
